e77c7e8e5fff3e839047e5171b676151185539b1
SSloan07 <159721122+SSloan07@users.noreply.github.com>
2026-05-01 20:25:30 -0500

    Pokenea model implemented

---
+++ models/pokeneasModel.js
@@ -0,0 +1,13 @@
+class Pokenea {
+
+  constructor(id, name, height, skill, philosophical_phrase){
+    this.id = id; 
+    this.name = name; 
+    this.height = height; 
+    this.skill = skill; 
+    this.philosophical_phrase = philosophical_phrase; 
+  }
+
+}
+
+module.exports = Pokenea; 
